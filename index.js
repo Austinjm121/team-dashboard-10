efcd8f8b334cf9ddb5beee18723ce1a4e98c61ee
Austinjm121 <austinjm121@gmail.com>
2022-08-10 15:41:22 -0500

    First commit, first files

---
+++ index.js
@@ -0,0 +1,12 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const Manager = require("./data/manager");
+const Engineer = require("./data/engineer");
+const Intern = require("./data/intern");
+
+
+
+
+
+
